edccb7890886bf11ec4ee7f6296518dc328c3e35
klu1622 <kathleenlu.26@gmail.com>
2016-11-02 21:43:30 -0500

    added line class

--- shapes.py
+++ shapes.py
@@ -82,3 +82,9 @@ class Triangle:
         print self.p2.point_tos()
         print self.p3.point_tos()
         print
+
+class Line():
+
+    def __init__(self, p1, p2):
+        self.p1 = p1
+        self.p2 = p2
